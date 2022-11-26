95069fddea53dab99ff041fc535b1f44edfa464b
kamrankamrankhan <lalitovelazquez1234@gmail.com>
2022-11-26 18:21:02 +0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # terraform-code-ec2
 create ec2 two instance using terraform 
+How to run the code 
+#terraform init 
+
+#terraform plan 
+
+#terraform apply --auto-approve
+
+#terraform destroy --auto-approve
---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region     = "us-east-1"
+  
+  
+}
+
+#Create a new EC2 launch configuration
+resource "aws_instance" "ec2_public" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  security_groups             = ["${aws_security_group.ssh-security-group.id}"]
+  subnet_id                   = aws_subnet.public-subnet-1.id
+  associate_public_ip_address = true
+  #user_data                   = "${data.template_file.provision.rendered}"
+  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    "Name" = "ec2-PUBLIC"
+  }
+  
+ 
+}
+#Create a new EC2 launch configuration
+resource "aws_instance" "ec2_private" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  security_groups             = ["${aws_security_group.webserver-security-group.id}"]
+  subnet_id                   = aws_subnet.private-subnet-1.id
+  associate_public_ip_address = false
+  #user_data                   = "${data.template_file.provision.rendered}"
+  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    "Name" = "EC2-PRIVATE"
+  }
+ 
+}
+################################
+ 
+
+
